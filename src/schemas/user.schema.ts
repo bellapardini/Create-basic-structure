dafa50405544d990d67525b0d56f1fbe5693abf5
bellapardini <74802820+bellapardini@users.noreply.github.com>
2022-12-14 01:04:26 -0300

    Create user.schema.ts

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,17 @@
+import Joi from "joi";
+
+export const loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(12).required(),
+});
+
+export const registerSchema = Joi.object({
+  email: Joi.string().email().required(),
+  name: Joi.string().min(3).required(),
+  password: Joi.string().min(12).required(),
+});
+
+export const scoreSchema = Joi.object({
+  id: Joi.string().required(),
+  score: Joi.number().min(0).required(),
+});
